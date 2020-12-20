60977944eac720ee9405b264e7ac911117e6848b
bhaktibhosale <75380210+bhaktibhosale@users.noreply.github.com>
2020-12-20 12:46:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
-function setup() {
-  createCanvas(800,400);
-  
-}
+const Engine = Matter.Engine; 
+const World = Matter.World; 
+const Bodies = Matter.Bodies;
 
+var engine,world;
+var ground;
+
+function setup() {
+  createCanvas(400,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  var ground_options={
+    isStatic:true
+  }
+  ground=Bodies.rectangle(200,390,200,20,ground_options)
+  World.add(world,ground);
+}
 function draw() {
   background(255,255,255);  
-  
+  Engine.uptade(engine);
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,20)
 }
